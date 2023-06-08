1bd49e39182ec8fc7a2459a96a1df55142c77bde
KiritoHashimoto <oleg88zverev@gmail.com>
2023-06-08 14:05:35 +0200

    12

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import random
 from aiogram import Bot, Dispatcher, executor, types
 from Gaben.secret import Api
 import pickle
+import atexit
 
 bot = Bot(Api)
 dp = Dispatcher(bot)
 
+def dump_file(USER_LIST):
+    with open("filename.pickle", "wb") as file_dump:
+        pickle.dump(USER_LIST, file_dump)
+
 def load_file_pkl():
     try:
-        with open("data.pkl", "rb") as file_load:
+        with open("filename.pickle", "rb") as file_load:
             res = pickle.load(file_load)
             return res
     except FileNotFoundError:
         return []
 
-USER_LIST = []
-
+read_file_word = open("words.txt", "r")
+already_read_file_word = read_file_word.read()
+words = already_read_file_word.split(",")
 class User:
     def __init__(self, id_chat):
         self.id_chat = id_chat
@@ -36,30 +42,10 @@ class User:
         user = User(chat_id)
         USER_LIST.append(user)
         return user
-                #todo: предвинуть две функции чтобы была как одна.
 
-
-
-
-
-
-# async def get_user(id_chat):
-#     for i in USER_LIST:
-#         if i.id_chat == id_chat:
-#             return i
-#
-#
-# async def create_user(id_chat):
-#     user = User(id_chat)
-#     USER_LIST.append(user)
-#     return user
-
-
-
-
+USER_LIST = load_file_pkl()
 
 async def get_old_word(message: types.Message, user):
-    global random
     real_word = user.word
     fake_word = list(real_word)
     for i in range(0, len(real_word), 2):
@@ -68,11 +54,7 @@ async def get_old_word(message: types.Message, user):
     await message.answer("".join(fake_word))
     await message.answer(real_word)
 
-
 async def give_new_word(message: types.Message, user):
-    with open("words.txt", "r") as read_file_word:
-        already_read_file_word = read_file_word.read()
-    words = already_read_file_word.split(",")
     real_word = random.choice(words)
     user.update_word(real_word)
     fake_word = list(real_word)
@@ -83,6 +65,11 @@ async def give_new_word(message: types.Message, user):
     await message.answer(real_word)
 
 
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.answer("Привет,\nв этом боте нужно отгадывать слова на английском\nчтобы начать нажми: /startik")
+
+
 @dp.message_handler(commands=["startik"])
 async def id_check(message: types.Message):
     chat_id = message.chat.id
@@ -90,31 +77,27 @@ async def id_check(message: types.Message):
     last_word = user.word
     if last_word is None:
         await give_new_word(message, user)
-        print("Give new word")  # Если слова нет
     else:
         await get_old_word(message, user)
-        print("Give old word")  # Если слово есть
-
 
 @dp.message_handler()
 async def check(message: types.Message):
     chat_id = message.chat.id
-    user = await get_user(chat_id)
+    user = User.get_or_create(chat_id)
     if message.text == user.word:
         wins = user.wins
         user.update_wins(wins + 1)
         user.update_word(None)
-        await message.answer(f"Ты выиграл уже: {user.wins} раз")
+        await message.answer(f"Ты выиграл уже: {user.wins} раз \n(/startik)")
+    elif user.word is None:
+        await message.answer(f"Чтобы получить слово нажми: /startik")
+    else:
+        await message.answer("Надо не так")
 
+@atexit.register
+def goodbye():
+    dump_file(USER_LIST)
+    read_file_word.close()
 
-try:
-    print("lolm")
+if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-finally:
-    print("fan")
-
-
-    with open("data.pkl", "w+b") as file_dump:
-        print(USER_LIST)
-        print("")
-        my_pickled_object = pickle.dump(USER_LIST, file_dump)
